ed6cb3fb8103cbccc8533312f75fc5ef749bc609
ankitnag0 <heroankit0@gmail.com>
2023-02-11 19:46:15 +0530

    reverted back to old user config and added authentication layer in all ednpoints except get individual things

--- backend/routes/contentRoutes.js
+++ backend/routes/contentRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createContent, deleteContent, getContentById, getContents, togglePublishStatus, updateContent } from "../controllers/contentController.js";
+import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router();
 
-router.post("/", createContent);
-router.get("/", getContents);
+router.post("/", protect, createContent);
+router.get("/", protect, getContents);
 router.get("/:id", getContentById);
-router.put("/:id", updateContent);
-router.delete("/:id", deleteContent);
-router.put("/status/:id", togglePublishStatus);
+router.put("/:id", protect, updateContent);
+router.delete("/:id", protect, deleteContent);
+router.put("/status/:id", protect, togglePublishStatus);
 
 export default router;
--- backend/routes/primaryLinkRoutes.js
+++ backend/routes/primaryLinkRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addPrimaryLink, deletePrimaryLink, getPrimaryLink, getPrimaryLinks, updatePrimaryLink } from "../controllers/primaryLinkController.js";
+import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router();
 
-router.post("/", addPrimaryLink);
-router.get("/", getPrimaryLinks);
+router.post("/", protect, addPrimaryLink);
+router.get("/", protect, getPrimaryLinks);
 router.get("/:id", getPrimaryLink);
-router.put("/:id", updatePrimaryLink);
-router.delete("/:id", deletePrimaryLink);
+router.put("/:id", protect, updatePrimaryLink);
+router.delete("/:id", protect, deletePrimaryLink);
 
 export default router;
